04156764e2602d56d59623a65862e88e776cadaa
Matthias Lim <matthiaslim2003@gmail.com>
2026-01-29 12:07:18 +0800

    Level 0

---
+++ ip-master/src/main/java/Jerry.java
@@ -0,0 +1,14 @@
+public class Jerry {
+    public static void main(String[] args) {
+        String botName = "Jerry";
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(" Hello! I'm " + botName);
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
